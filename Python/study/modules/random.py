1f26faf39f65f3a82d3a743467a29af5c3f8f836
Yliu <39017281+Yong90@users.noreply.github.com>
2018-05-08 22:30:59 +0800

    Create random.py

---
+++ Python/study/modules/random.py
@@ -0,0 +1,18 @@
+import random
+ 
+print(random.random())#(0,1)----float
+ 
+print(random.randint(1,3))  #[1,3]
+ 
+print(random.randrange(1,3)) #[1,3)
+ 
+print(random.choice([1,'23',[4,5]]))#23
+ 
+print(random.sample([1,'23',[4,5]],2))#[[4, 5], '23']
+ 
+print(random.uniform(1,3))#1.927109612082716
+ 
+ 
+item=[1,3,5,7,9]
+random.shuffle(item)
+print(item)

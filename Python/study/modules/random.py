e4ea6454ad976859d452cbb6696a2f0e56f34d6f
Yliu <39017281+Yong90@users.noreply.github.com>
2018-05-08 22:32:37 +0800

    Update random.py

--- Python/study/modules/random.py
+++ Python/study/modules/random.py
@@ -16,3 +16,22 @@ print(random.uniform(1,3))#1.927109612082716
 item=[1,3,5,7,9]
 random.shuffle(item)
 print(item)
+
+
+#######  验证码
+import random
+
+def v_code():
+
+    code = ''
+    for i in range(5):
+
+        num=random.randint(0,9)
+        alf=chr(random.randint(65,90))
+        add=random.choice([num,alf])
+        code += str(add)
+    return code
+
+print(v_code())
+
+验证码
